bb5b3a40966d4e29b108452cfc307f7fc5b0f022
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-02-26 05:38:30 -0300

    Iniciado código do jogo
    Finalizado ajuste upper e strip

--- forca.py
+++ forca.py
@@ -4,4 +4,32 @@ def jogar():
     print("***Bem vindo ao jogo da forca***")
     print('********************************')
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_",]
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra?")
+        chute = chute.strip() #Eliminar espações em branco na hora da pesquisa e não causar erro
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()): #upper() convente a string em maiúscula 
+                letras_acertadas[index] = letra
+            index = index + 1
+        
+
+        print(letras_acertadas)
+
+
+
+
     print("Fim de jogo!")
+
+if(__name__ == "__main__"):
+    jogar()
